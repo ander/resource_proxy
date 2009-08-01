866d81509fc883d47b5a11f507278162c8791cc9
ander <antti.hakala@gmail.com>
2009-08-01 22:56:47 +0300

    register RailsCacheProxy

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 # Include hook code here
 
 ResourceProxy.register(:ar, ActiveRecordProxy)
+ResourceProxy.register(:cache, RailsCacheProxy)
 ResourceProxy.register(:session, SessionProxy)
 
 ActiveRecord::Base.send(:extend, ResourceProxy::Finder)
